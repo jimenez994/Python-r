8a601edaf151afd6712f9e64c4e27eecb0d27276
Jonathan Jimenez <j.jimenez.994@hotmail.com>
2017-11-30 15:52:54 -0500

    reorder of files

---
+++ myEnvironments/hello_world.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+                                          # render_template to allow us to render index.html.
+app = Flask(__name__)                     # Global variable __name__ tells Flask whether or not we
+                                          # are running the file directly or importing it as a module.
+@app.route('/')                           # The "@" symbol designates a "decorator" which attaches the
+                                          # following function to the '/' route. This means that
+                                          # whenever we send a request to localhost:5000/ we will run
+                                          # the following "hello_world" function.
+def hello_world():
+  # Render the template and return it!
+  return render_template('yellow_belt_test.html')
+app.run(debug=True)
