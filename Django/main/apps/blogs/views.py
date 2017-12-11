d6f71e1d90cccd04f6e07e0406bef0bcc94f49d2
Jonathan Jimenez <j.jimenez.994@hotmail.com>
2017-12-11 18:43:58 -0500

    Django fundamenteal

---
+++ Django/main/apps/blogs/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, HttpResponse, redirect
+
+# Create your views here.
+def index(request):
+    response = "placeholder to later display all the list of blogs"
+    return HttpResponse(response)
+def new(response):
+    response = "placeholder to display a new form to create a new blog"
+    return HttpResponse(response)
+def create(response):
+    response = "create"
+    return redirect('/')
+def show(request, number):
+    print number 
+    return HttpResponse("placeholder to display blog " + number)
+def edit(request, number):
+    print number 
+    return HttpResponse("placeholder to edit blog " + number)
+def destroy(request, number):
+    print number
+    return redirect('/')
