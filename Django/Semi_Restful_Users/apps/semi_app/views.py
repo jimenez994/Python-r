c66c9046af9ec438cba0683e6d0e0bc79acf1bf9
Jonathan Jimenez <j.jimenez.994@hotmail.com>
2017-12-15 20:04:16 -0500

    Semi Restful Users

--- Django/Semi_Restful_Users/apps/semi_app/views.py
+++ Django/Semi_Restful_Users/apps/semi_app/views.py
@@ -1,4 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import User
+
 
 def index(request):
-    return render(request,"semi_app/index.html")
+    users = User.objects.all()
+
+    return render(request,"semi_app/index.html",{"users": users})
+
+
+def edit(request,id):
+    users = User.objects.get(id=id)
+    return render(request, "semi_app/edit.html", {"users":users})
+
+def update(request,id):
+    user = User.objects.get(id=id)
+    user.name = request.POST["name"]
+    user.save()
+    return redirect("/")
+def new(request):
+        
+    return render(request, "semi_app/new.html")
+def create(request):
+    User.objects.create(
+        name=request.POST["name"],
+        email=request.POST["email"]
+
+    )
+    return redirect("/")
+
+def info(request,id):
+    users = User.objects.get(id=id)
+    return render(request, "semi_app/info.html", {"users":users})
+
+def delete(request,id):
+    User.objects.get(id=id).delete()
+    return redirect("/")
