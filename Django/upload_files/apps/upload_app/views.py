e13446869e526e450c50ae3244a9369d66006004
Jonathan Jimenez <j.jimenez.994@hotmail.com>
2017-12-23 18:13:00 -0500

    Upload_app fully workinggit add -A

--- Django/upload_files/apps/upload_app/views.py
+++ Django/upload_files/apps/upload_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .models import *
 from .forms import *
 
+
 IMAGE_FILE_TYPES = ['png', 'jpg', 'jpeg']
 
 def index(request):
@@ -10,9 +11,25 @@ def index(request):
 
 def upload(request):
     form = imageForm(request.POST or None, request.FILES or None)
-    
-    if form.is_valid():
-        form.save()
-        return redirect('/')
- 
-    return render(request, 'upload_app/upload.html',{'form': form })
+    if request.FILES.get('image') != None:
+        if form.is_valid():
+            post = form.save()
+            post.image = request.FILES['image']
+            file_type = post.image.url.split('.')[-1]
+            file_type = file_type.lower()
+            if file_type not in IMAGE_FILE_TYPES:
+                context = {
+                    'form': form,
+                    'error_message': 'image must me jpg'
+                }
+                return render(request, 'upload_app/index.html',context)
+            post.save()
+            return redirect("/")
+    else:
+        if form.is_valid():
+            form.save()
+            return redirect("/")
+        else:
+            form = imageForm()
+        return render(request, 'upload_app/upload.html',{'form': form})
+
