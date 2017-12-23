7c319e6c8ee80b4aa5d0d39f36cac77dc45615c0
Jonathan Jimenez <j.jimenez.994@hotmail.com>
2017-12-23 12:43:06 -0500

    testing simple image upload

--- projects/bo/bo/urls.py
+++ projects/bo/bo/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from uploads.core import views
+
 
 urlpatterns = [
     url(r'^', include("apps.bo_app.urls")),
